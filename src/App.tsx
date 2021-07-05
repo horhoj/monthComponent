06b7b5afe813d9fff35f025339538a9a84fbd4ca
horhoj <horhoj@yandex.ru>
2021-07-05 09:13:21 +0500

    add default css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 1024px;
+`;
 
 export const App: React.FC = () => {
-  return <div>App</div>;
+  return <Container>App</Container>;
 };
